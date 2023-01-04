9fc56ca73bd97cc3c68603347dad363b5963f639
gcdesigner <gcdesigner.contato@gmail.com>
2023-01-04 17:30:59 -0300

    fix database connection

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,23 +1,33 @@
 import { DataSource } from "typeorm";
 
 import { Category } from "../modules/cars/entities/Category";
+import { Specification } from "../modules/cars/entities/Specification";
 import { CreateCategories1672793250607 } from "./migrations/1672793250607-CreateCategories";
+import { CreateSpecifications1672846730011 } from "./migrations/1672846730011-CreateSpecifications";
 
-export const appDataSource = new DataSource({
+const appDataSource = new DataSource({
   type: "postgres",
-  host: "database",
   port: 5432,
   username: "docker",
   password: "ignite",
   database: "rentx",
   synchronize: true,
   logging: true,
-  entities: [Category],
+  entities: [Category, Specification],
   subscribers: [],
-  migrations: [CreateCategories1672793250607],
+  migrationsRun: true,
+  migrations: [
+    CreateCategories1672793250607,
+    CreateSpecifications1672846730011,
+  ],
 });
 
-appDataSource
-  .initialize()
-  .then(() => console.log("Database initialized!"))
-  .catch((err) => console.log("Database error", err));
+function createConection(host = "database") {
+  return appDataSource
+    .setOptions({ host })
+    .initialize()
+    .then(() => console.log("Database initialized!"))
+    .catch((err) => console.log("Database error", err));
+}
+
+export { appDataSource, createConection };
