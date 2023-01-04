3fcdcf54dcba2dbe443572c1dc2b585ae40bf515
gcdesigner <gcdesigner.contato@gmail.com>
2023-01-04 12:08:43 -0300

    added tsyringe - improve dependency injectable

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,13 +1,19 @@
+/* eslint-disable prettier/prettier */
+
+import { inject, injectable } from "tsyringe";
+
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
   name: string;
   description: string;
 }
-
+@injectable()
 class CreateCategoryUseCase {
-  // eslint-disable-next-line prettier/prettier
-  constructor(private categoriesRepository: ICategoriesRepository) { }
+  constructor(
+    @inject("CategoriesRepository")
+    private categoriesRepository: ICategoriesRepository
+  ) { }
 
   async execute({ name, description }: IRequest) {
     const categoryExists = await this.categoriesRepository.findByName(name);
--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -1,13 +1,13 @@
-import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
-import { CreateCategoryController } from "./CreateCategoryController";
-import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+// import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+// import { CreateCategoryController } from "./CreateCategoryController";
+// import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
-export default () => {
-  const categoriesRepository = new CategoriesRepository();
-  const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
-  const createCategoryController = new CreateCategoryController(
-    createCategoryUseCase
-  );
+// export default () => {
+//   const categoriesRepository = new CategoriesRepository();
+//   const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
+//   const createCategoryController = new CreateCategoryController(
+//     createCategoryUseCase
+//   );
 
-  return createCategoryController;
-};
+//   return createCategoryController;
+// };
